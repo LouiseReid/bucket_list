89a6587f93e9a63ecfa37ed61b7302bca7fb6035
Jamie Sime <jamiesime@gmail.com>
2017-12-13 10:51:48 +0000

    amended alias for start to begin nodemon

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -67,6 +67,26 @@
 /* 0 */
 /***/ (function(module, exports) {
 
+var app = function(){
+    var url = "https://restcountries.eu/rest/v2/all";
+    makeRequest(url, requestComplete);
+};
+
+var makeRequest = function(url, callback){
+    request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', callback);
+    request.send();
+};
+
+var requestComplete = function(){
+    if(this.status!=200){return};
+    var jsonString = this.responseText;
+    var countyList = JSON.parse(jsonString);
+}
+
+
+window.addEventListener("load", app);
 
 
 /***/ })
