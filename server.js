6577fdb945ac8c3f638e48d2177a5e5cd56d4b7c
LouiseReid <l_reid@hotmail.co.uk>
2017-12-13 11:50:21 +0000

    countries route working

--- server.js
+++ server.js
@@ -10,14 +10,23 @@ app.use(express.static('client/build'));
 var MongoClient = require("mongodb").MongoClient;
 
 
-MongoClient.connect("mongodb://localhost:27017/bucketList", function(err, database){
+MongoClient.connect("mongodb://localhost:27017/bucketList", function(err, client){
   if (err){
     return console.log(err);
   }
-  db = database;
+  db = client.db('bucketList');
   console.log("connected to db");
 
   app.listen(3000, function(){
     console.log("Listening on port 3000");
   });
 });
+
+app.get('/countries', function(req, res){
+  db.collection('countries').find().toArray(function(err, results){
+    if(err){
+      return console.log(err);
+    }
+    res.json(results);
+  });
+})
