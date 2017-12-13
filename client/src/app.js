49080af4481b132aa414a0331510d9773f78bc7d
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-13 11:12:37 +0000

    Selector dropdown now populated and bundle.js added to html

--- client/src/app.js
+++ client/src/app.js
@@ -13,8 +13,18 @@ var makeRequest = function(url, callback){
 var requestComplete = function(){
     if(this.status!=200){return};
     var jsonString = this.responseText;
-    var countyList = JSON.parse(jsonString);
+    var countryList = JSON.parse(jsonString);
+    populateSelect(countryList)
 }
+//
+var populateSelect = function(countryList) {
+  var select = document.getElementById("countries-list");
+  countryList.forEach(function(country){
+    var option = document.createElement('option');
+    option.innerText = country.name;
+    select.appendChild(option);
+  })
 
+}
 
 window.addEventListener("load", app);
