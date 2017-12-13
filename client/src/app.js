20e0179b13f4b64779c6c14a03d89852b8d1f2a0
Jamie Sime <jamiesime@gmail.com>
2017-12-13 13:59:16 +0000

    Sidebar map has user dropdown and list beneath which populates with the countries that match their name in api

--- client/src/app.js
+++ client/src/app.js
@@ -2,8 +2,15 @@ var map = require('./mapWrapper')
 
 var app = function(){
     var url = "https://restcountries.eu/rest/v2/all";
-    makeRequest(url, requestComplete);
+    makeRequest(url, requestAllCountries);
+    var url = "http://localhost:3000/countries";
+    makeRequest(url, requestUsers);
     displayMap();
+    var userDropper = document.getElementById('user-dropper');
+    userDropper.addEventListener("change", function(){
+      makeRequest(url, requestUserCountries);
+    });
+
 };
 
 var makeRequest = function(url, callback){
@@ -13,14 +20,29 @@ var makeRequest = function(url, callback){
     request.send();
 };
 
-var requestComplete = function(){
+var requestAllCountries = function(){
     if(this.status!=200){return};
     var jsonString = this.responseText;
     var countryList = JSON.parse(jsonString);
-    populateSelect(countryList)
+    populateCountries(countryList)
 }
+
+var requestUsers = function(){
+    if(this.status!=200){return};
+    var jsonString = this.responseText;
+    var userList = JSON.parse(jsonString);
+    populateUserlist(userList)
+}
+
+var requestUserCountries = function(userName){
+    if(this.status!=200){return};
+    var jsonString = this.responseText;
+    var bucketList = JSON.parse(jsonString);
+    populateBucketList(bucketList)
+}
+
 //
-var populateSelect = function(countryList) {
+var populateCountries = function(countryList) {
   var select = document.getElementById("countries-list");
   countryList.forEach(function(country){
     var option = document.createElement('option');
@@ -29,11 +51,33 @@ var populateSelect = function(countryList) {
   })
 }
 
+var populateUserlist = function(userList){
+  var select = document.getElementById("user-dropper");
+  userList.forEach(function(user){
+    var option = document.createElement('option');
+    option.innerText = user.name;
+    select.appendChild(option);
+  })
+}
+
+var populateBucketList = function(bucketList){
+  var userName = document.getElementById("user-dropper").value;
+  var bList = document.getElementById("user-countries")
+  bucketList.forEach(function(item){
+      if (item.name === userName){
+        var liTag = document.createElement('li');
+        liTag.innerText = item.country;
+        bList.appendChild(liTag);
+      }
+  });
+}
+
 var displayMap = function(){
   var map = document.getElementById('map-container');
   var center = {lat: 0, lng: 0};
   var mainMap = new MapWrapper(map, center, 5);
 };
+
 
 
 window.addEventListener("load", app);
